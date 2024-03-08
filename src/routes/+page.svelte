e06ead65f4245fcbbfc6787279cbdaf20de7f9ac
victormhp <victoralbertomhp@gmail.com>
2024-03-08 13:05:01 -0600

    Add main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { Particles } from '$lib/particles';
+  import { NavHome } from '$lib/ui';
+  import { isMenuOpened } from '$lib/store';
+
+  const routes = [{ title: 'Sorting Algorithms', img: './sorting.png', path: '/sorting' }];
+</script>
+
+<svelte:head>
+  <title>Code Craft</title>
+</svelte:head>
+
+<NavHome />
+<main class="relative flex min-h-screen w-full items-center justify-center">
+  <Particles />
+  <section
+    aria-hidden={!$isMenuOpened}
+    class="grid grid-cols-[repeat(auto-fill,minmax(16rem,1fr))] place-items-center gap-4 px-8 pb-10 pt-20 aria-hidden:hidden lg:gap-8 lg:pb-16 lg:pt-32"
+  >
+    {#each routes as { title, img, path }}
+      <article class="rounded-lg border border-zinc-200/60 bg-zinc-50 p-4 shadow-md">
+        <div class="px-2 py-4">
+          <a href={path}>
+            <img src={img} alt={title} />
+          </a>
+        </div>
+        <h2 class="text-center font-virgil text-xl">{title}</h2>
+      </article>
+    {/each}
+  </section>
+</main>
