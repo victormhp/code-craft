a8f391d23f5d3c8065bed82a832cfdab72a97c84
victormhp <victoralbertomhp@gmail.com>
2024-03-08 13:02:16 -0600

    Add linter, formatter and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    fontFamily: {
+      'virgil': ['Virgil', 'Arial', 'sans-serif'],
+      'orange': ['Cartoon', 'Arial', 'sans-serif'],
+      'roboto': ['Roboto', 'Arial', 'sans-serif']
+    },
+    extend: {}
+  },
+  plugins: []
+};
