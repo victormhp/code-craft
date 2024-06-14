fc84dd4c2accb05b80922cb660d86b6e80c71a80
victormhp <victoralbertomhp@gmail.com>
2024-06-14 13:42:19 -0600

    Add view transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,19 @@
 <script>
   import '../app.css';
+  import { onNavigate } from '$app/navigation';
+
+  onNavigate((navigation) => {
+    // @ts-expect-error View Transitions
+    if (!document.startViewTransition) return;
+
+    return new Promise((resolve) => {
+    // @ts-expect-error View Transitions
+      document.startViewTransition(async () => {
+        resolve();
+        await navigation.complete;
+      });
+    });
+  });
 </script>
 
 <slot />
