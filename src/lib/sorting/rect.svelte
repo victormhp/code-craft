27b3bc41f651013de59405f613fe773a4e6a3cb9
victormhp <victoralbertomhp@gmail.com>
2024-06-16 22:43:36 -0600

    Remove green color when sorted

--- src/lib/sorting/rect.svelte
+++ src/lib/sorting/rect.svelte
@@ -4,37 +4,14 @@
   export let id = 0;
   export let width = 0;
   export let height = 0;
-  export let move: number[] = [];
 
-  let isSorted = false;
-
-  // Track the move index in which the rect is correctly sorted.
-  let sortedMoveIndex = 0;
 
   $: isMoving = $sortingState.move.includes(id) && $canStep && $canStepBack;
-  $: {
-    if (move.length == 2 && id === move[0] && id === move[1]) {
-      sortedMoveIndex = $sortingState.current;
-      isSorted = true;
-    } else if ($sortingState.current < 1 || $sortingState.current < sortedMoveIndex) {
-      sortedMoveIndex = $sortingState.current;
-      isSorted = false;
-    }
-
-    if (move.length > 2) {
-      if ($sortingState.current + 1 === $sortingState.total) {
-        isSorted = true;
-      } else {
-        isSorted = false;
-      }
-    }
-  }
 </script>
 
 <div
   data-moving={isMoving}
-  data-sorted={isSorted}
-  class="relative flex items-center justify-center rounded-t-md border border-white bg-zinc-800 data-[moving=true]:bg-red-500 data-[sorted=true]:bg-green-500"
+  class="relative flex items-center justify-center rounded-t-md border border-white bg-zinc-800 data-[moving=true]:bg-red-500"
   style="width: {width}px; height: {height}px"
 >
   {#if $sortingShowValues}
