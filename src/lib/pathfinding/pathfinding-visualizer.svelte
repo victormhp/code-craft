ece11cfca119204bc4b5fd84d50f876d1425056f
victormhp <victoralbertomhp@gmail.com>
2024-06-24 01:14:42 -0600

    Add scroll on pathfinding toolbar

--- src/lib/pathfinding/pathfinding-visualizer.svelte
+++ src/lib/pathfinding/pathfinding-visualizer.svelte
@@ -2,8 +2,8 @@
   import 'iconify-icon';
   import Node from './node.svelte';
 
-  let gridWidth: number = 0;
-  let gridHeight: number = 0;
+  let gridWidth = 0;
+  let gridHeight = 0;
   let nodeSize = 25;
 
   const pathfindingAlgorithms = ['Dijkstra', 'A Star', 'Breath First Search', 'Depth First Search'];
@@ -17,9 +17,9 @@
 </script>
 
 <div
-  class="flex flex-col justify-between items-center gap-2 rounded-lg border border-zinc-200 bg-zinc-100 p-2 shadow-sm sm:flex-row"
+  class="flex flex-row items-center justify-between gap-2 overflow-x-auto whitespace-nowrap rounded-lg border border-zinc-200 bg-zinc-100 px-4 py-2 shadow-sm"
 >
-  <div class="flex gap-4">
+  <div class="flex shrink-0 flex-row items-center gap-4">
     <div class="flex flex-col items-start gap-1">
       <label class="pl-1 text-sm text-zinc-500" for="pathfinding">Algorithm</label>
       <select id="pathfinding" class="rounded border border-zinc-200 bg-zinc-50 p-2">
@@ -28,7 +28,6 @@
         {/each}
       </select>
     </div>
-
     <div class="flex flex-col items-start gap-1">
       <label class="pl-1 text-sm text-zinc-500" for="maze">Maze</label>
       <select id="maze" class="rounded border border-zinc-200 bg-zinc-50 p-2">
@@ -37,23 +36,6 @@
         {/each}
       </select>
     </div>
-  </div>
-  <div class="flex items-center gap-2">
-    <label for="node-size">Node Size</label>
-    <div class="flex gap-2">
-      <input
-        id="node-size"
-        class="grow"
-        name="node-size"
-        type="range"
-        bind:value={nodeSize}
-        min="20"
-        max="60"
-      />
-      <p class="rounded border border-zinc-300 px-2 py-1 shadow-sm">{nodeSize}</p>
-    </div>
-  </div>
-  <div>
     <button
       class="rounded border bg-zinc-200 px-4 py-2 transition-colors hover:bg-zinc-300"
       type="button"
@@ -65,6 +47,15 @@
       type="button"
     >
       <p class="font-bold">Clear Board</p>
+    </button>
+  </div>
+  <div class="shrink-0">
+    <button
+      class="rounded-full border bg-zinc-800 p-2 transition-colors hover:bg-zinc-700 focus-visible:bg-zinc-700"
+      type="button"
+    >
+      <iconify-icon icon="mingcute:play-fill" width="20" height="20" style="color: #fafafa"
+      ></iconify-icon>
     </button>
   </div>
 </div>
