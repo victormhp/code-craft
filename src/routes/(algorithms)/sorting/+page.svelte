aad99110279d08db5525c19c420501b2753dcfc6
victormhp <victoralbertomhp@gmail.com>
2024-06-26 01:35:06 -0600

    Update sorting page

--- src/routes/(algorithms)/sorting/+page.svelte
+++ src/routes/(algorithms)/sorting/+page.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
   import { SortingSettings, SortingVisualizer } from '$lib/components/sorting';
-
-  let size = 15;
-  let delay = 100;
-  let isHeightVisible = false;
 </script>
 
 <svelte:head>
@@ -14,7 +10,7 @@
   class="grid grid-cols-1 grid-rows-[1fr,1fr] gap-8 px-4 py-8 lg:grid-cols-[2fr,5fr] lg:grid-rows-1 lg:p-16"
 >
   <div class="max-lg:order-last">
-    <SortingSettings bind:size bind:delay bind:isHeightVisible />
+    <SortingSettings />
   </div>
-  <SortingVisualizer {size} {delay} {isHeightVisible} />
+  <SortingVisualizer />
 </main>
